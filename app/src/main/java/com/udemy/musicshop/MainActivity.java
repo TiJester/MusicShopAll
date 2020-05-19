7d0a04ddc7dacce058cf5ea5368b624679e35f99
Gregorii work <tijester@ukr.net>
2020-05-19 17:25:37 +0300

    Quantity + Item + Image

--- app/src/main/java/com/udemy/musicshop/MainActivity.java
+++ app/src/main/java/com/udemy/musicshop/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     //
     int quantity = 0;
@@ -18,12 +21,17 @@ public class MainActivity extends AppCompatActivity {
     ArrayList spinnerArrayList;
     ArrayAdapter spinnerAdapter;
 
+    HashMap goodsMap;
+    String goodsName;
+    double price;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
         spinnerArrayList = new ArrayList();
 
         spinnerArrayList.add("guitar");
@@ -33,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(spinnerAdapter);
+
+        goodsMap = new HashMap();
+        goodsMap.put("guitar", 100.00);
+        goodsMap.put("drums", 200.00);
+        goodsMap.put("keyboard", 300.00);
     }
 
     public void quantityMinus(View view) {
@@ -42,11 +55,45 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView minusTextView = findViewById(R.id.quantityTextView);
         minusTextView.setText("" + quantity);
+        TextView priceTextView =findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
     }
 
     public void quantityPlus(View view) {
         quantity = quantity + 1;
         TextView plusTextView = findViewById(R.id.quantityTextView);
         plusTextView.setText("" + quantity);
+        TextView priceTextView =findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double) goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+        switch (goodsName){
+            case "guitar":
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+            case "drums":
+                goodsImageView.setImageResource(R.drawable.drums);
+                break;
+            case "keyboard":
+                goodsImageView.setImageResource(R.drawable.keyboard);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.guitar);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
