9e280cb952b5c2e24c9bf2bfd4142604a8eb42a7
Gregorii work <tijester@ukr.net>
2020-05-22 13:55:42 +0300

    Add class Order

--- app/src/main/java/com/udemy/musicshop/MainActivity.java
+++ app/src/main/java/com/udemy/musicshop/MainActivity.java
@@ -3,9 +3,11 @@ package com.udemy.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -25,11 +27,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     String goodsName;
     double price;
 
+    EditText userNameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createMap();
+        createSpinner();
+
+        userNameEditText = findViewById(R.id.nameEditText);
+    }
+
+    void createSpinner(){
         spinner = findViewById(R.id.spinner);
         spinner.setOnItemSelectedListener(this);
         spinnerArrayList = new ArrayList();
@@ -41,7 +52,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(spinnerAdapter);
+    }
 
+    void createMap(){
         goodsMap = new HashMap();
         goodsMap.put("guitar", 100.00);
         goodsMap.put("drums", 200.00);
@@ -95,5 +108,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void addToCart(View view) {
+
+        Order order = new Order();
+        order.userName = userNameEditText.getText().toString();
+        order.goodsName = goodsName;
+        order.quantity = quantity;
+        order.orderPrice = quantity * price;
+
+        Log.d("userName", order.userName);
+        Log.d("goodsName", order.goodsName);
+        Log.d("quantityName", "" + order.quantity);
+        Log.d("orderPriceName", "" + order.orderPrice);
     }
 }
---
+++ app/src/main/java/com/udemy/musicshop/Order.java
@@ -0,0 +1,9 @@
+package com.udemy.musicshop;
+
+public class Order {
+
+    String userName;
+    String goodsName;
+    int quantity;
+    double orderPrice;
+}
