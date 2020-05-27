6ea78c4f2de2878c3ed0ef83f6dea24aa9a46af9
Gregorii work <tijester@ukr.net>
2020-05-27 12:41:41 +0300

    New activity to email

--- app/src/main/java/com/udemy/musicshop/OrderActivity.java
+++ app/src/main/java/com/udemy/musicshop/OrderActivity.java
@@ -3,10 +3,16 @@ package com.udemy.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String[] addresses = {"admin@bzv.com.ua"};
+    String subject = "Order from Music Shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +27,25 @@ public class OrderActivity extends AppCompatActivity {
         int quantity = receivedOrderIntent.getIntExtra("quantityForIntent", 0);
         double orderPriceForIntent = receivedOrderIntent.getDoubleExtra("orderPriceForIntent", 0);
         double priceForIntent = receivedOrderIntent.getDoubleExtra("priceForIntent", 0);
+        emailText = "Customer name: " + userName +"\n" +
+                "Goods name: " + goodsName + "\n" +
+                "Quantity: "+ quantity + "\n" +
+                "Price: "+ priceForIntent + "\n" +
+                "Order Price: "+ orderPriceForIntent;
+
         TextView userNameText = findViewById(R.id.orderTextView);
-        userNameText.setText("Customer name: " + userName +
-                "\n Goods name: "+ goodsName +
-                "\n Quantity: "+ quantity +
-                "\n Price: "+ priceForIntent +
-                "\n Order Price: "+ orderPriceForIntent);
+        userNameText.setText(emailText);
     }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
